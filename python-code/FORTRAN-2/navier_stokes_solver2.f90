ad97fe99b188eb482e9c4aed15d62090aebd7f68
emiliotorres17 <emiliotorres17@yahoo.com>
2020-06-08 14:12:56 -0700

    newest version of the code

---
+++ python-code/FORTRAN-2/navier_stokes_solver2.f90
@@ -0,0 +1,134 @@
+program navier_stokes
+    use precision_m    
+    use ns_lib 
+    real(WP)                                        :: xs = 0.0_WP, xf = 1.0_WP
+    integer, parameter                              :: xlen = 101
+    integer, parameter                              :: M = 16, N = 16
+    real(WP), dimension(1:xlen)                     :: xvec
+    real(WP)                                        :: dx, dy, nu
+    real(WP), dimension(0:N)                        :: ul, ur, vl, vr
+    real(WP), dimension(0:M)                        :: ub, ut, vb, vt
+    real(WP), dimension(0:N, 0:M-1)                 :: u, ustar
+    real(WP), dimension(0:N-1,0:M)                  :: v, vstar
+    real(WP), dimension(0:N, 0:M-1)                 :: Lu
+    real(WP), dimension(0:N, 0:M-1)                 :: Nx
+    real(WP), dimension(0:N-1, 0:M)                 :: Lv, Ny
+    real(WP),dimension(0:N, 0:M)                    :: P
+    real(WP)                                        :: maxerror
+    integer                                         :: i, j, nstep, tstep_F
+    real(WP)                                        :: tmax, t
+    integer                                         :: counter
+    real(WP)                                        :: max_div
+    real(WP)                                        :: div
+    !---------------------------------------------------------------------!
+    ! Linear derivative test                                              !
+    !---------------------------------------------------------------------!
+    counter     = 1
+    u           = 0.0_WP
+    v           = 0.0_WP
+    ustar       = 0.0_WP
+    vstar       = 0.0_WP
+    ut          = 1.0_WP
+    ub          = 0.0_WP
+    ur          = 0.0_WP
+    ul          = 0.0_WP
+    vt          = 0.0_WP
+    vb          = 0.0_WP
+    vr          = 0.0_WP
+    vl          = 0.0_WP
+    nu          = 0.01_WP
+    dx          = 1.0_WP/real(M)
+    dy          = 1.0_WP/real(N)
+    dt          = 0.25_WP*dx*dy/nu/2.0_WP
+    tmax        = 25.0_WP
+    maxerror    = (10.0_WP)**(-12.0_WP)
+    tstep_F     = int(1.0000001_WP*tmax/dt)
+    print *, tstep_F
+    !---------------------------------------------------------------------!
+    ! Time stepping loop                                                  !
+    !---------------------------------------------------------------------!
+    do nstep  = 1, tstep_F
+        t   = t + dt   
+        call time_derv(Lu, Lv, Nx, Ny, M, N, dx, dy, nu, u, v, ul, ur, ub, ut, &
+                                    vl, vr, vb, vt)
+        !-----------------------------------------------------------------!
+        ! Updating ustar and vstar                                        !
+        !-----------------------------------------------------------------!
+        do j = 1, N
+            do i  = 1, M-1
+                ustar(j,i) = u(j,i) + dt*(-Nx(j,i) + Lu(j,i))
+            end do
+        end do
+        do j = 1, N-1
+            do i = 1, M
+                vstar(j,i) = v(j,i) + dt*(-Ny(j,i) + Lv(j,i))
+            end do
+        end do
+        !-----------------------------------------------------------------!
+        ! Updating pressure                                               !
+        !-----------------------------------------------------------------!
+        call calcpress(P, M, N, dx, dy, maxerror, ustar, vstar, ul, &
+                                ur, ub, ut, vl, vr, vb, vt)
+        !-----------------------------------------------------------------!
+        ! Updating u and v                                                !
+        !-----------------------------------------------------------------!
+        do j  = 1, N
+            do i = 1, M-1
+                u(j,i) = ustar(j,i) - (P(j,i+1)-P(j,i))/dx
+            end do
+        end do 
+        do j  = 1, N-1
+            do i  = 1, M
+                v(j,i) = vstar(j,i) - (P(j+1,i)-P(j,i))/dy
+            end do 
+        end do
+        !-----------------------------------------------------------------!
+        ! Calculating velocity divergence                                 !
+        !-----------------------------------------------------------------!
+        call divergence(max_div, div, M, N, u, v, ul, ur, ut, ub, &
+                            vl, vr, vt, vb, dx, dy)
+        !-----------------------------------------------------------------!
+        ! Print statement                                                 !
+        !-----------------------------------------------------------------!
+        print '(I8, A, I8, 4X, A, f10.6, 4X, A, ES10.3)', &
+                nstep, '/', tstep_F, &
+                'time -->', t, &
+                'max velocity -->', max(maxval(u),maxval(v))
+        counter = 0                                                                          
+        !-----------------------------------------------------------------!
+        ! Divergence print out                                            !
+        !-----------------------------------------------------------------!
+        print '(I8, A , I8, A, ES16.5)', &
+            nstep, '/', tstep_F, &
+            '| Maximum velocity diveregence:', max_div
+        !-----------------------------------------------------------------!
+        ! Writing output                                                  !
+        !-----------------------------------------------------------------!
+        10 format(I8, A, I8, 4X, A, f10.6, 4X, A, ES10.3, /, I8, A, I8, 4X, A, f10.6, 4X, A, ES10.3)
+        open(unit=1, file='output.out')
+        write(1,  10)&
+                nstep, '/', tstep_F, &
+                'time -->', t, &
+                'max velocity -->', max(maxval(u),maxval(v)), &
+                nstep, '/', tstep_F, &
+                '| Maximum velocity diveregence:', max_div
+            
+    end do
+    close(unit=1)
+    !---------------------------------------------------------------------!
+    ! writing u-velocity                                                  !
+    !---------------------------------------------------------------------!
+    open(unit=1, file='u-velocity.dat')
+    do i = 0, M-1
+        write(1,*) ( u(j,i), j=0,N)
+    end do
+    close(unit=1)
+    !---------------------------------------------------------------------!
+    ! writing u-velocity                                                  !
+    !---------------------------------------------------------------------!
+    open(unit=1, file='v-velocity.dat')
+    do i = 0, M
+        write(1,*) ( v(j,i), j=0,N-1)
+    end do
+    close(unit=1)
+end program navier_stokes
